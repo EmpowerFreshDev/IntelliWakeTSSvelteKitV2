6449954b3f53471014426630705ebacc47198650
Dennis Peters <denjpeters@gmail.com>
2023-12-14 19:52:40 -0500

    Add project settings and update Vite configuration
    Several IDE specific settings files have been added to enforce consistent code style and inspection rules. The Vite configuration has been updated to include mkcert plugin, port settings, and host settings. A bun package file has also been updated.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert'
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), mkcert()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		port: 5183,
+		https: true,
+		host: '0.0.0.0',
+		strictPort: false
+	},
+	preview: {
+		port: 5282,
+		https: true,
+		host: '0.0.0.0',
+		strictPort: false
 	}
 });
