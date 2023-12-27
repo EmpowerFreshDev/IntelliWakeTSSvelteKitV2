b61d21605977767419827f1a6eb535e71a4a5ea2
Dennis Peters <denjpeters@gmail.com>
2023-12-26 21:26:17 -0500

    Update tsconfig, add DisplayColorStyle logic and upgrade dependencies
    The module configuration in tsconfig.json file has been set to "ESNext". A new logic for DisplayColorStyle has been defined in DisplayColorStyle.ts. The project dependencies in package.json have been updated which includes adding '@solidbasisventures/intelliwaketsfoundation' and upgrading '@sveltejs/package', '@typescript-eslint/eslint-plugin', and '@typescript-eslint/parser' to their respective latest versions. All these changes have been

--- src/lib/DisplayColorStyle.ts
+++ src/lib/DisplayColorStyle.ts
@@ -1,3 +1,15 @@
+import {isNullUndefined} from '@solidbasisventures/intelliwaketsfoundation'
+
+/**
+ * Options for specifying display color style.
+ *
+ * @typedef {Object} TDisplayColorStyleOptions
+ * @property {string} [prefix] - The prefix to be added to the color.
+ * @property {string} [suffix] - The suffix to be added to the color.
+ * @property {number} [saturationPercent] - The saturation level of the color (between 0 and 1).
+ * @property {number} [lightnessPercent] - The lightness level of the color (between 0 and 1).
+ * @property {number} [alpha] - The alpha value of the color (between 0 and 1).
+ */
 export type TDisplayColorStyleOptions = {
 	prefix?: string
 	suffix?: string
@@ -9,19 +21,18 @@ export type TDisplayColorStyleOptions = {
 	alpha?: number
 }
 
-
 /**
- * Create hsl style
+ * Converts the given hue, saturation, and lightness values to a display color style string.
  *
- * @param hue
- * @param saturation
- * @param lightness
- * @param options
- * @constructor
+ * @param {number | null | undefined} hue - The hue value.
+ * @param {number | null | undefined} saturation - The saturation value.
+ * @param {number | null | undefined} lightness - The lightness value.
+ * @param {TDisplayColorStyleOptions} [options] - The options to customize the display color style.
+ * @returns {string | undefined} - The display color style string, or undefined if the values are invalid.
  */
-export const DisplayColorStyle = (hue: number | null, saturation: number | null, lightness: number | null,
+export const DisplayColorStyle = (hue: number | null | undefined, saturation: number | null | undefined, lightness: number | null | undefined,
                                   options?: TDisplayColorStyleOptions): string | undefined => {
-	if (hue == null || saturation == null || lightness == null) {
+	if (isNullUndefined(hue) == null || saturation == null || lightness == null) {
 		return undefined
 	}
 
