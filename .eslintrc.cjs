1708a0eeda23b3cbc6606ec5ea3b592578e4b19d
Dennis Peters <denjpeters@gmail.com>
2024-01-05 20:27:30 -0500

    Fix export issues

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -27,5 +27,19 @@ module.exports = {
 				parser: '@typescript-eslint/parser'
 			}
 		}
-	]
-};
+	],
+	'rules': {
+		"@typescript-eslint/no-empty-interface": "off",
+		'@typescript-eslint/no-empty-function': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/explicit-module-boundary-types': 'off',
+		'eqeqeq': 'off',
+		'require-await': 'off',
+		'no-async-promise-executor': 'off',
+		'svelte/no-at-html-tags': 'off'
+		// "@typescript-eslint/ban-types": "off",
+		// "@typescript-eslint/no-explicit-any": "off",
+		// "@typescript-eslint/explicit-module-boundary-types": "off",
+		// "require-await": "off"
+	}
+}
