cace06ed35bd6fafdf7433cd44f48fda23c1e03c
Dennis Peters <denjpeters@gmail.com>
2024-01-16 17:51:04 -0500

    Implement read-only mode in Fractions page
    Introduced a read-only mode for the Fractions page's InputNumberScroll component. A switch has been added to toggle between the editable and read-only state. In read-only mode, changes to the input value are prevented. Additional elements like an increment button have also been included for when the component is not in read-only mode.

--- src/routes/Fractions/+page.svelte
+++ src/routes/Fractions/+page.svelte
@@ -1,15 +1,24 @@
 <script lang='ts'>
     import InputNumberScroll from '$lib/InputNumberScroll.svelte'
     import DisplayFraction from '$lib/DisplayFraction.svelte'
+    import Switch from '$lib/Switch.svelte'
 
     let value = 1.25
+
+    let readonly = false
 </script>
 
 <div class='grid grid-cols-3 overflow-hidden'>
     <div>
         <input type='number' step={0.25} bind:value/>
+	    <Switch bind:checked={readonly}>
+		    Read Only
+	    </Switch>
+	    <button type='button' on:click={() => value++}>
+		    +1
+	    </button>
     </div>
-    <InputNumberScroll bind:value lowerRange={0} upperRange={20} increment={1/3} saveManualEntryMode="TestMEM"/>
+    <InputNumberScroll bind:value lowerRange={0} upperRange={20} increment={1/3} saveManualEntryMode="TestMEM" {readonly}/>
     <div>
         <DisplayFraction {value}/>
     </div>
