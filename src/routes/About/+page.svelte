32284c8907faacc9584a699751bfee9115dfc1d4
Dennis Peters <denjpeters@gmail.com>
2024-09-13 17:09:10 -0400

    Update about page

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -3,24 +3,28 @@
 	import { abouts } from './AboutData'
 </script>
 
-<div class='p-4 mx-auto max-w-[35rem] overflow-y-auto'>
-	<h1 class='text-center'>IntelliWake SvelteKit 2 Library</h1>
-	<p class='text-center mb-4'>
+<div class='p-4 mx-auto max-w-[45rem] overflow-hidden grid grid-rows-[auto_1fr]'>
+	<div>
+		<h1 class='text-center'>IntelliWake SvelteKit 2 Library</h1>
+		<p class='text-center mb-4'>
 
-	</p>
+		</p>
+	</div>
 
-	<h1>All</h1>
-	<About {abouts} />
+<!--	<div class="overflow-y-auto">-->
+<!--		<h1>All</h1>-->
+		<About {abouts} />
 
-	<h1>Limit 5 days</h1>
-	<About {abouts} filterBackAdjustment={{days: 5}} />
+<!--		<h1>Limit 5 days</h1>-->
+<!--		<About {abouts} filterBackAdjustment={{days: 5}} />-->
 
-	<h1>Limit 4 minors</h1>
-	<About {abouts} filterBackMinorCount={4} />
+<!--		<h1>Limit 4 minors</h1>-->
+<!--		<About {abouts} filterBackMinorCount={4} />-->
 
-	<h1>Limit 2 minors</h1>
-	<About {abouts} filterBackMinorCount={2} />
+<!--		<h1>Limit 2 minors</h1>-->
+<!--		<About {abouts} filterBackMinorCount={2} />-->
 
-	<h1>Limit 5 days / 2 minors</h1>
-	<About {abouts} filterBackAdjustment={{days: 5}} filterBackMinorCount={2} />
+<!--		<h1>Limit 5 days / 2 minors</h1>-->
+<!--		<About {abouts} filterBackAdjustment={{days: 5}} filterBackMinorCount={2} />-->
+<!--	</div>-->
 </div>
